c83765ffb2573c8b155919c5d9b015ea3b45ff04
Oliver Beatson <oliverbeatson@gmail.com>
2016-06-09 15:33:02 +0100

    Footer component

---
+++ app/components/Footer/index.js
@@ -0,0 +1,16 @@
+/*
+ * Footer
+ */
+
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class Footer extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <footer>
+        <p>There will be stuff in the footer</p>
+      </footer>
+    );
+  }
+}
