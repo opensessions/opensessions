6d879990a66afdb7d6adcdc6ff504cda580c21bb
Oliver Beatson <oliverbeatson@gmail.com>
2016-06-09 15:34:43 +0100

    remove duplicate Headers

--- app/containers/ForgotPage/index.js
+++ app/containers/ForgotPage/index.js
@@ -4,15 +4,12 @@
 
 import React from 'react';
 
-import Header from 'components/Header';
-
 import { Authenticated, NotAuthenticated, ResetPasswordForm } from 'react-stormpath';
 
 export default class ForgotPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div>
-        <Header />
         <NotAuthenticated>
           <p>Send an email</p>
           <ResetPasswordForm />
