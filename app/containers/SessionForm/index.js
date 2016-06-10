0f6be1f9e22c85bfdeccf782ab4795acbe71ff13
Oliver Beatson <oliverbeatson@gmail.com>
2016-06-10 17:19:41 +0100

    Sessopm form

--- app/containers/SessionForm/index.js
+++ app/containers/SessionForm/index.js
@@ -17,13 +17,12 @@ export default class SessionForm extends React.Component { // eslint-disable-lin
   render() {
     return (
       <div className={styles.form}>
-        <div>
+        <div className={styles.titleBar}>
           <h2>Add a session</h2>
           <h3>Title</h3>
           <Link to="/session/view/ExampleSessionID" className={styles.previewButton}>Preview</Link>
         </div>
         <Authenticated>
-          <h1>A session form!</h1>
           <Form autosave="true">
             <Fieldset label="Description">
               <Field label="Title" name="title" />
