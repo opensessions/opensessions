65963878bd0b976cb963f699bb1969cd394f2ed8
Nick Evans <nick@imin.co>
2016-08-19 00:09:54 +0100

    Minor fix to analytics util

--- app/utils/analytics.js
+++ app/utils/analytics.js
@@ -7,7 +7,7 @@ import { IntercomAPI } from 'react-intercom';
 export default function trackPage(href, pathname) {
   const { analytics } = window;
   if (analytics) {
-    const generalise = (str) => !str ? '' : str.replace(/^(.*session\/)([^\/]*)((?:\/edit)?.*)$/gi, '$1*$3');
+    const generalise = (str) => !str ? '' : str.replace(/^(.*session\/)([^\/#]*)((?:\/edit)?.*)$/gi, '$1*$3');
     const generalHref = generalise(href);
     const generalPathname = generalise(pathname);
     analytics.page(generalPathname, {
