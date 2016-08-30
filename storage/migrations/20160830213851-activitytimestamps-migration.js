131ff4b56c7b09d7133fee8a311450dea6f35fe8
Oliver Beatson <oliverbeatson@gmail.com>
2016-08-30 22:45:39 +0100

    final migration

---
+++ storage/migrations/20160830213851-activitytimestamps-migration.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  up: (migration, Sequelize) => {
+    return migration.addColumn('Activities', 'createdAt', { type: Sequelize.DATE }).then(() => {
+      return migration.addColumn('Activities', 'updatedAt', { type: Sequelize.DATE });
+    });
+  },
+  down: (migration) => {
+    return migration.removeColumn('createdAt').then(() => {
+      return migration.removeColumn('updatedAt');
+    });
+  }
+};
