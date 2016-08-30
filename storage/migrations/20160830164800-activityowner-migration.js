80d01f8dcc7d7d10f0c4ffd746621c8c7191de21
Oliver Beatson <oliverbeatson@gmail.com>
2016-08-30 17:49:34 +0100

    activity owner column

---
+++ storage/migrations/20160830164800-activityowner-migration.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  up: (migration, Sequelize) => {
+    return migration.addColumn('Activities', 'owner', { type: Sequelize.STRING });
+  },
+  down: (migration) => {
+    return migration.removeColumn('Activities', 'owner');
+  }
+};
