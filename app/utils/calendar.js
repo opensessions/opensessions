567a3232a8bb187d0081f141bb65c3f3c0df2358
Oliver Beatson <oliverbeatson@gmail.com>
2016-10-13 13:19:46 +0100

    fix past display wrong

--- app/utils/calendar.js
+++ app/utils/calendar.js
@@ -7,19 +7,18 @@ export function parseSchedule(instance) {
   const data = {};
   const now = new Date();
   if (startDate) {
-    const date = new Date(startDate);
-    data.date = moment(date).format('dddd D MMM');
-    if (date.getFullYear() !== now.getFullYear()) data.date = moment(date).format('dddd D MMM YYYY');
+    const date = new Date(startTime ? [startDate, startTime].join('T') : startDate);
+    data.date = moment(date).format(date.getFullYear() === now.getFullYear() ? 'dddd D MMM' : 'dddd D MMM YYYY');
     data.hasOccurred = date.getTime() <= now.getTime();
   }
   if (startTime) {
-    data.time = startTime.split(':').slice(0, 2).join(':');
+    data.time = startTime.slice(0, 5);
     if (endTime) {
       const dur = moment.duration(moment(pgTimeToDate(endTime)).diff(moment(pgTimeToDate(startTime))));
       const hours = (dur.asHours() + 24) % 24;
       const hoursInt = Math.floor(hours);
       const minsInt = Math.ceil((hours % 1) * 60);
-      data.duration = `${hoursInt ? `${hoursInt}h` : ''}${minsInt ? ` ${minsInt}m` : ''}`;
+      data.duration = [hoursInt ? `${hoursInt}h` : '', minsInt ? ` ${minsInt}m` : ''].join('');
     }
   }
   return data;
