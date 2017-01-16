c97f2c403caf2ffdc7aa2188aff42fa01747657b
Oliver Beatson <oliverbeatson@gmail.com>
2017-01-16 14:42:56 +0000

    analytics page

--- app/containers/Dashboard/index.js
+++ app/containers/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react';
-import { Chart, LineChart } from 'react-d3-basic';
+import { LineChart } from 'react-d3-basic';
 
 import LoadingMessage from '../../components/LoadingMessage';
 
@@ -7,21 +7,24 @@ import { apiFetch, apiModel } from '../../utils/api';
 
 import styles from './styles.css';
 
-const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-const getMonthName = date => `${monthNames[date.getMonth()]}${date.getMonth() ? '' : ` ${date.getFullYear()}`}`;
+// const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+// const getMonthName = date => `${monthNames[date.getMonth()]}${date.getMonth() ? '' : ` ${date.getFullYear()}`}`;
+
+const MS_PER_DAY = 1000 * 60 * 60 * 24;
+const intervalsAgo = (date, interval) => Math.floor((Date.now() - date.getTime()) / (MS_PER_DAY * interval));
 
 export default class Dashboard extends React.Component { // eslint-disable-line react/prefer-stateless-function
   static contextTypes = {
     store: PropTypes.object,
     notify: PropTypes.func
   };
-  static fetchData(dispatch, query) {
+  static fetchData(dispatch) {
     return apiModel.search('session', { state: 'published' }).then(result => {
       const { instances, error } = result;
       if (error) throw error;
       dispatch({ type: 'SESSION_LIST_LOADED', payload: instances });
-      return apiFetch('/api/admin/users').then(result => {
-        dispatch({ type: 'USER_LIST_LOADED', payload: result.users });
+      return apiFetch('/api/admin/users').then(userResult => {
+        dispatch({ type: 'USER_LIST_LOADED', payload: userResult.users });
       });
     });
   }
@@ -30,60 +33,70 @@ export default class Dashboard extends React.Component { // eslint-disable-line 
     this.state = { isLoading: false, users: null };
   }
   componentDidMount() {
-    if (!this.props.sessions) {
-      this.setState({ isLoading: true }); // eslint-disable-line react/no-did-mount-set-state
-      this.constructor.fetchData(this.context.store.dispatch, this.props.location.query).then(() => {
-        this.setState({ isLoading: false });
-      }).catch(error => {
-        this.context.notify(error, 'error');
-      });
-    }
+    this.setState({ isLoading: true }); // eslint-disable-line react/no-did-mount-set-state
+    this.constructor.fetchData(this.context.store.dispatch).then(() => {
+      this.setState({ isLoading: false });
+    }).catch(error => {
+      this.context.notify(error, 'error');
+      this.setState({ isLoading: false });
+    });
   }
   renderChart(sessions, period, interval) {
     if (!sessions) return null;
     return (<div className={styles.chart}>
-      <h1>Published sessions per {interval > 1 ? `${interval}-day period` : 'day'}</h1>
-      {this.renderTimeChartFromCreatedObjects(sessions, 'updatedAt', period, interval)}
+      <h2>Published sessions per {interval > 1 ? `${interval}-day period` : 'day'}</h2>
+      {this.renderTimeChartFromCreatedObjects(sessions, 'updatedAt', period, interval, 'session')}
+    </div>);
+  }
+  renderSessionAnalytics() {
+    const sessions = this.context.store.getState().get('sessionList');
+    if (!sessions) return <div>Failed to load session information</div>;
+    return (<div className={styles.chart}>
+      <h1>Session Publishing Analytics</h1>
+      <p>Total sessions: {sessions.length}</p>
+      {this.renderChart(sessions, 42, 1)}
+      {this.renderChart(sessions, 26, 7)}
     </div>);
   }
   renderUserAnalytics() {
     const users = this.context.store.getState().get('userList');
     if (!users) return <div>Failed to load user information</div>;
-    return <div className={styles.chart}>
+    return (<div className={styles.chart}>
       <h1>User Analytics</h1>
       <p>Total users: {users.length}</p>
-      {this.renderTimeChartFromCreatedObjects(users, 'created_at', 26, 7)}
-    </div>;
+      <p>Active users (online in last 28 days): {users.filter(user => intervalsAgo(new Date(user.last_login), 1) > 28).length}</p>
+      {this.renderTimeChartFromCreatedObjects(users, 'created_at', 26, 7, 'user')}
+    </div>);
   }
-  renderTimeChartFromCreatedObjects(objects, timeField, period, interval) {
+  renderTimeChartFromCreatedObjects(objects, timeField, intervalCount, interval, name) {
     if (!objects) return null;
-    const periods = (new Array(period)).join(',').split(',').map(v => 0);
-    const MS_PER_INTERVAL = 1000 * 60 * 60 * 24 * interval;
-    const intervalsAgo = date => Math.floor((Date.now() - date.getTime()) / MS_PER_INTERVAL);
+    const periods = (new Array(intervalCount)).join(',').split(',').map(() => 0);
+    let uncounted = 0;
     objects.forEach(object => {
-      const interval = intervalsAgo(new Date(object[timeField]));
-      if (interval in periods) periods[interval] = periods[interval] ? periods[interval] + 1 : 1;
+      const intervalDiff = intervalsAgo(new Date(object[timeField]), interval);
+      if (intervalDiff in periods) periods[intervalDiff] = periods[intervalDiff] ? periods[intervalDiff] + 1 : 1;
+      else uncounted++;
     });
-    const data = periods.map((val, key) => ({ day: new Date((Date.now() - (key * MS_PER_INTERVAL))), total: val }));
-    return <LineChart
-      width={960}
-      height={320}
-      data={data}
-      showXGrid={true}
-      showYGrid={true}
-      chartSeries={[{ field: 'total', name: 'total', color: '#88C540', style: { 'stroke-width': 2 } }]}
-      x={day => day ? day.day || 0 : 0}
-      xScale="time"
-    />;
+    const data = periods.map((val, key) => ({ time: new Date(Date.now() - (key * (MS_PER_DAY * interval))), total: val }));
+    return (<div>
+      <LineChart
+        width={960}
+        height={320}
+        data={data}
+        showXGrid
+        showYGrid
+        chartSeries={[{ field: 'total', name: 'total', color: '#88C540', style: { 'stroke-width': 2 } }]}
+        x={period => period.time}
+        xScale="time"
+      />
+      {uncounted ? <p>({uncounted} {name}s not plotted)</p> : null}
+    </div>);
   }
   render() {
     const { isLoading } = this.state;
-    const sessions = this.context.store.getState().get('sessionList');
     return (<div>
       {isLoading ? <LoadingMessage message="Loading sessions" ellipsis /> : null}
-      {sessions ? this.renderChart(sessions, 28, 1) : null}
-      {/*sessions ? this.renderChart(sessions, 180, 1) : null*/}
-      {sessions ? this.renderChart(sessions, 26, 7) : null}
+      {this.renderSessionAnalytics()}
       {this.renderUserAnalytics()}
     </div>);
   }
