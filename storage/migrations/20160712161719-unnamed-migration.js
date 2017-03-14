31613bb54486f900278e4e62c15a5c011036af02
Luke Winship <luke.winship@gmail.com>
2017-03-14 16:04:12 +0000

    added OrganizerUuid to initial migration

--- storage/migrations/20160712161719-unnamed-migration.js
+++ storage/migrations/20160712161719-unnamed-migration.js
@@ -2,64 +2,73 @@
 
 module.exports = {
   up: (migration, DataTypes) => {
-    return migration.createTable('Sessions', {
+    return migration.createTable('Organizers', {
       uuid: {
         type: DataTypes.UUID,
         defaultValue: DataTypes.UUIDV1,
         primaryKey: true
       },
-      state: {
-        type: DataTypes.STRING,
-        defaultValue: 'draft',
-        validation: {
-          isIn: ['draft', 'published', 'unpublished', 'deleted']
-        }
-      },
       owner: DataTypes.STRING,
-      title: DataTypes.STRING(50),
-      description: DataTypes.STRING(2048),
-      activityType: DataTypes.STRING,
-      preparation: DataTypes.STRING(2048),
-      leader: DataTypes.STRING,
-      hasCoaching: DataTypes.BOOLEAN,
-      location: DataTypes.STRING,
-      locationData: DataTypes.JSON,
-      meetingPoint: DataTypes.STRING,
-      attendanceType: DataTypes.STRING,
-      price: DataTypes.FLOAT(2),
-      quantity: DataTypes.INTEGER,
-      genderRestriction: {
-        type: DataTypes.STRING(16),
-        defaultValue: 'mixed',
-        validation: {
-          isIn: ['mixed', 'male', 'female']
+      name: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: true
         }
       },
-      minAgeRestriction: DataTypes.INTEGER,
-      maxAgeRestriction: DataTypes.INTEGER,
-      contactPhone: DataTypes.STRING,
-      contactEmail: DataTypes.STRING,
-      endTime: DataTypes.TIME,
-      startDate: DataTypes.DATE,
-      startTime: DataTypes.TIME,
       createdAt: DataTypes.DATE,
       updatedAt: DataTypes.DATE
     }).then(() => (
-      migration.createTable('Organizers', {
+      migration.createTable('Sessions', {
         uuid: {
           type: DataTypes.UUID,
           defaultValue: DataTypes.UUIDV1,
           primaryKey: true
         },
-        owner: DataTypes.STRING,
-        name: {
+        state: {
           type: DataTypes.STRING,
-          validate: {
-            notEmpty: true
+          defaultValue: 'draft',
+          validation: {
+            isIn: ['draft', 'published', 'unpublished', 'deleted']
           }
         },
+        owner: DataTypes.STRING,
+        title: DataTypes.STRING(50),
+        description: DataTypes.STRING(2048),
+        activityType: DataTypes.STRING,
+        preparation: DataTypes.STRING(2048),
+        leader: DataTypes.STRING,
+        hasCoaching: DataTypes.BOOLEAN,
+        location: DataTypes.STRING,
+        locationData: DataTypes.JSON,
+        meetingPoint: DataTypes.STRING,
+        attendanceType: DataTypes.STRING,
+        price: DataTypes.FLOAT(2),
+        quantity: DataTypes.INTEGER,
+        genderRestriction: {
+          type: DataTypes.STRING(16),
+          defaultValue: 'mixed',
+          validation: {
+            isIn: ['mixed', 'male', 'female']
+          }
+        },
+        minAgeRestriction: DataTypes.INTEGER,
+        maxAgeRestriction: DataTypes.INTEGER,
+        contactPhone: DataTypes.STRING,
+        contactEmail: DataTypes.STRING,
+        endTime: DataTypes.TIME,
+        startDate: DataTypes.DATE,
+        startTime: DataTypes.TIME,
         createdAt: DataTypes.DATE,
-        updatedAt: DataTypes.DATE
+        updatedAt: DataTypes.DATE,
+        OrganizerUuid: {
+          type: DataTypes.UUID,
+          references: {
+            model: 'Organizers',
+            key: 'uuid'
+          },
+          onUpdate: 'cascade',
+          onDelete: 'set null'
+        }
       })
     ))
   },
