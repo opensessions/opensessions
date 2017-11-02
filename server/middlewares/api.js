387452e655863f75d51ba3941617c5df532543cc
JMurphyWeb <jmurphy.web@gmail.com>
2017-11-02 16:09:26 +0000

    #462 - remove analytics snippet and intercom from project

--- server/middlewares/api.js
+++ server/middlewares/api.js
@@ -215,11 +215,6 @@ module.exports = (database) => {
     const windowKeys = ['GOOGLE_MAPS_API_KEY', 'SEGMENT_WRITE_KEY', 'AWS_S3_IMAGES_BASEURL', 'AUTH0_CLIENT_ID', 'AUTH0_CLIENT_DOMAIN', 'LOCALE_COUNTRY', 'ADMIN_DOMAIN', 'SENTRY_DSN_PUBLIC'];
     res.send(`
       ${windowKeys.map(key => `window["${key}"] = '${process.env[key]}'`).join(';\n')};
-
-      !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.1.0";
-        if (window.SEGMENT_WRITE_KEY) analytics.load(window.SEGMENT_WRITE_KEY);
-      }}();
-
       function addScript(src) {
         var script = document.createElement('script');
         script.src = src;
